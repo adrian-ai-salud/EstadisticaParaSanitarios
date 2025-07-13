be656680b98788f2ee615192bc5ec06de6501e1c
adrian-ia-salud <adrian.vences.garrido@gmail.com>
2025-07-13 17:29:00 +0200

    fix(HazardRatioVisualizer): Eliminar importaciones de recharts no utilizadas

--- src/components/HazardRatioVisualizer.tsx
+++ src/components/HazardRatioVisualizer.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, ReferenceLine } from 'recharts';
+
 
 interface HazardRatioData {
   name: string;
